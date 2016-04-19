87827af3ce7932c582c733c0812a2bbe368e5d37
Burin Choomnuan <agilecreativity@gmail.com>
2016-04-19 22:25:39 +1000

    Initial import

---
+++ test/clojure_walkthrough/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-walkthrough.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-walkthrough.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
