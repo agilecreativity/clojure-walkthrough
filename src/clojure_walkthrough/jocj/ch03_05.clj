6c8a0fc37c9af2e6a42c7591e24528ce1aad95ad
Burin Choomnuan <agilecreativity@gmail.com>
2016-04-20 10:54:25 +1000

    Add more examples

---
+++ src/clojure_walkthrough/jocj/ch03_05.clj
@@ -0,0 +1,24 @@
+(ns clojure-walkthrough.jocj.ch03-05
+  (:require [clojure.data.json :as json]
+            [clojure.xml :as xml-core])
+  (:gen-class))
+
+;; See: https://github.com/clojure/data.json
+(def sample-json 
+  (json/write-str {:a 1 :b 2}))
+
+sample-json ;; "{\"a\":1,\"b\":2}"
+
+(json/read-str sample-json) ;; {"a" 1, "b" 2}
+
+;;  e.g. 3.8:
+(declare load-totals)
+
+(defn import-xml-transactions-from-bank [url]
+  (let [xml-document (xml-core/parse url)]
+    ;; more code here
+    ))
+
+(defn totals-by-day [start-date end-date]
+  (let [expenses-by-day (load-totals start-date end-date)]
+    (json/json-str expense-by-day)))
