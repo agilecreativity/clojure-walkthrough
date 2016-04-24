4932d09b543da1b81725f1c85aae56154e705636
Burin Choomnuan <agilecreativity@gmail.com>
2016-04-24 14:54:42 +1000

    Add more examples

--- src/clojure_walkthrough/cjia/ch05_xx.clj
+++ src/clojure_walkthrough/cjia/ch05_xx.clj
@@ -52,3 +52,42 @@
 
 (. rnd (nextInt 10)) ;; 3 ;; result will vary
 (. rnd (nextInt 10)) ;; 6 ;; result will vary
+
+(import '(java.util Calendar TimeZone))
+
+;; Compare this 
+(. (. (Calendar/getInstance) getTimeZone) getDisplayName) ;; "Australian Eastern Standard Time (Victoria)"
+
+;; To this
+(.. (Calendar/getInstance) (getTimeZone) (getDisplayName)) ;; "Australian Eastern Standard Time (Victoria)"
+
+;; This can be simplified to this
+(.. (Calendar/getInstance) getTimeZone getDisplayName)     ;; "Australian Eastern Standard Time (Victoria)"
+
+;; If using method signaturs that accepted arguments
+(.. (Calendar/getInstance)
+  getTimeZone
+  (getDisplayName true TimeZone/SHORT)) ;; "AEDT"
+
+;; Look at this example without the 'doto' macro
+(import '(java.util Calendar))
+(defn the-past-midnight-1 []
+ (let [calendar-obj (Calendar/getInstance)]
+  (.set calendar-obj Calendar/AM_PM Calendar/AM)
+  (.set calendar-obj Calendar/HOUR 0)
+  (.set calendar-obj Calendar/MINUTE 0)
+  (.set calendar-obj Calendar/SECOND 0)
+  (.set calendar-obj Calendar/MILLISECOND 0)
+  (.getTime calendar-obj))) ;; #'clojure-walkthrough.cjia.ch05-xx/the-past-midnight-1
+
+;; With the use of 'doto' macro
+(import '(java.util Calendar))
+(defn the-past-midnight-2 []
+  (let [calendar-obj (Calendar/getInstance)]
+    (doto calendar-obj
+      (.set Calendar/AM_PM Calendar/AM)
+      (.set Calendar/HOUR 0)
+      (.set Calendar/MINUTE 0)
+      (.set Calendar/SECOND 0)
+      (.set Calendar/MILLISECOND 0))
+    (.getTime calendar-obj)))
