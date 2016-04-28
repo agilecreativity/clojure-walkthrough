9489afa563eadb86e4b92062d9b1133cee299e9a
Burin Choomnuan <agilecreativity@gmail.com>
2016-04-28 18:13:03 +1000

    Show example with macroexpand

--- src/clojure/clojure_walkthrough/cjia/ch07_xx.clj
+++ src/clojure/clojure_walkthrough/cjia/ch07_xx.clj
@@ -234,3 +234,4 @@ nv(login-user request) ; "Welcome back, amit, 1234is correct!"
 
 (assert-true (< (* 4 5) (* 2 5))) ;; java.lang.RuntimeException
                                   ;; (* 4 5) is not < 10
+(macroexpand-1 '(assert-true (>= (* 2 4) (/ 18 2)))) ;; (clojure.core/let [rhsv__20666__auto__ (/ 18 2) ret__20667__auto__ (>= (* 2 4) (/ 18 2))] (clojure.core/if-not ret__20667__auto__ (throw (java.lang.RuntimeException. (clojure.core/str (quote (* 2 4)) " is not " (quote >=) " " rhsv__20666__auto__))) true))
