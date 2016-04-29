3fa7bf3854b1eace723672e9a85a495446c7e2b3
Burin Choomnuan <agilecreativity@gmail.com>
2016-04-30 09:13:58 +1000

    Add more examples (continue)

--- src/clojure/clojure_walkthrough/cjia/ch08_xx.clj
+++ src/clojure/clojure_walkthrough/cjia/ch08_xx.clj
@@ -393,3 +393,51 @@
     (case command
       :name (name class-name)
       :new (new-object klass))))
+
+(defmacro defclass [class-name & specs]
+  (let [fns (or (method-specs specs) {})]
+    `(def ~class-name (new-class '~class-name ~fns))))
+
+(defclass Person
+  (method age [] (* 2 10))
+  (method greet [visitor] (str "Hello there, " visitor)))
+
+;; Invoking methods
+(defn find-method [method-name instance-methods]
+  (instance-methods method-name))
+
+(defn new-class [class-name methods]
+  (fn klass [command & args]
+    (case command
+      :name (name class-name)
+      :new (new-object klass)
+      :method (let [[method-name] args]
+                (find-method method-name methods)))))
+
+(Person :method :age) ; #function[clojure-walkthrough.cjia.ch08-xx/age--20738]
+
+((Person :method :age)) ; 20
+
+(defn new-object [klass]
+  (let [state (ref {})]
+    (fn [command & args]
+      (case command
+        :class klass
+        :class-name (klass :name)
+        :set! (let [[k v] args]
+                (dosync (alter state assoc k v))
+                nil)
+        :get (let [[k v] args]
+               (@state key))
+        (if-let [method (klass :method command)]
+          (apply method args)
+          (throw (RuntimeException.
+                  (str "Unable to respond to " command))))))))
+
+(def shelly (Person :new))
+
+(shelly :age) ; 20
+
+(shelly :greet "Nancy") ;; "Hello there, Nancy"
+
+;; Referring to this (TBC)
