01690f6c9cd4596d95171d1e8bc1c576aa5366d7
Burin Choomnuan <agilecreativity@gmail.com>
2016-04-30 18:17:28 +1000

    Add more examples

--- src/clojure/clojure_walkthrough/cjia/ch08_xx.clj
+++ src/clojure/clojure_walkthrough/cjia/ch08_xx.clj
@@ -441,3 +441,101 @@
 (shelly :greet "Nancy") ;; "Hello there, Nancy"
 
 ;; Referring to this (TBC)
+
+(declare ^:dynamic this)
+
+(defn new-object [klass]
+  (let [state (ref {})]
+    (fn thiz [command & args]
+      (case command
+        :class klass
+        :class-name (klass :name)
+        :set! (let [[k v] args]
+                (dosync (alter state assoc k v))
+                nil)
+        :get (let [[key] args] (@state key))
+        (let [method (klass :method command)]
+          (if-not method
+            (throw (RuntimeException.
+                    (str "Unable to respond to " command))))
+          (binding [this thiz]
+            (apply method args)))))))
+
+(defclass Person
+  (method age [] (* 2 10))
+  (method about [diff]
+          (str "I was born about " (+ diff (this :age)) " years ago")))
+
+(def shelly (Person :new))
+
+(shelly :about 2) ; "I was born about 22 years ago"
+
+;; Class inheritance
+(defclass Woman
+  (extends Person)
+  (method greet [v] (str "Hello, " v))
+  (method age [] (* 2 9)))
+
+(defn parent-class-spec [sexprs]
+  (let [extends-spec (filter #(= 'extends (first %)) sexprs)
+        extends (first extends-spec)]
+    (if (empty? extends)
+      'OBJECT
+      (last extends))))
+
+(parent-class-spec '((extends Person)
+                     (method age [] (* 2 9)))) ;; Person
+
+(var map) ; #'clojure.core/map
+
+(defmacro defclass [class-name & specs]
+  (let [parent-class (parent-class-spec specs)
+        fns (or (method-specs specs) {})]
+    `(def ~class-name (new-class '~class-name #'~parent-class ~fns))))
+
+(defn new-class [class-name parent methods]
+  (fn klass [command & args]
+    (case command
+      :name (name class-name)
+      :parent parent
+      :new (new-object klass)
+      :method (let [[method-name] args]
+                (find-method method-name methods)))))
+
+(def OBJECT (new-class :OBJECT nil {}))
+
+(defclass Person
+  (method age [] (* 2 10))
+  (method about [diff]
+          (str "I was born about " (+ diff (this :age)) " years ago")))
+
+(defclass Woman
+  (extends Person)
+  (method greet [v] (str "Hello, " v))
+  (method age [] (* 2 9)))
+
+(def donna (Woman :new))
+
+(donna :greet "Shelly") ;; "Hello, Shelly"
+
+(donna :age) ;; 18
+
+;; (donna :about 3) ;; Unable respond to :about ..
+
+(defn find-method [method-name klass]
+  (or ((klass :methods) method-name)
+      (if-not (= #'OBJECT klass)
+        (find-method method-name (klass :parent)))))
+
+(defn new-class [class-name parent methods]
+  (fn klass [command & args]
+    (case command
+      :name (name class-name)
+      :parent parent
+      :new (new-object klass)
+      :methods methods
+      :method (let [[method-name] args]
+                (find-method method-name klass)))))
+
+;; (donna :about 3)
+
