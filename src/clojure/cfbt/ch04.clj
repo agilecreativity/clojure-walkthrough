e0f09dcc773ad8f71777cad31c61ab24b1a91fed
Burin Choomnuan <agilecreativity@gmail.com>
2016-05-02 08:33:47 +1000

    Add more examples

--- src/clojure/cfbt/ch04.clj
+++ src/clojure/cfbt/ch04.clj
@@ -88,4 +88,24 @@
 (take-while #(< (:month %) 4)
             (drop-while #(< (:month %) 2) food-journal)) ;; ({:month 2, :day 1, :human 4.9, :critter 2.1} {:month 2, :day 2, :human 5.0, :critter 2.5} {:month 3, :day 1, :human 4.2, :critter 3.3} {:month 3, :day 2, :human 4.0, :critter 3.8})
 
-;; Filter and some! (TBC)
+;; Filter and some
+
+(filter #(< (:human %) 5) food-journal) ;; ({:month 2, :day 1, :human 4.9, :critter 2.1} {:month 3, :day 1, :human 4.2, :critter 3.3} {:month 3, :day 2, :human 4.0, :critter 3.8} {:month 4, :day 1, :human 3.7, :critter 3.9} {:month 4, :day 2, :human 3.7, :critter 3.6})
+
+(filter #(< (:month %) 3) food-journal) ;; ({:month 1, :day 1, :human 5.3, :critter 2.3} {:month 1, :day 2, :human 5.1, :critter 2.0} {:month 2, :day 1, :human 4.9, :critter 2.1} {:month 2, :day 2, :human 5.0, :critter 2.5})
+
+(some #(> (:critter %) 5) food-journal) ;; nil
+
+(some #(> (:critter %) 3) food-journal) ;; true
+
+(some #(and (> (:critter %) 3) %) food-journal) ;; {:month 3, :day 1, :human 4.2, :critter 3.3}
+
+;; sort and sort-by
+(sort [3 1 2]) ;; (1 2 3)
+
+(sort-by count ["aaa" "c" "bb"]) ;; ("c" "bb" "aaa")
+
+;; concat
+(concat [1 2] [3 4]) ;;(1 2 3 4)
+
+;; Lazy sequences
