21669710a6dbd4eceb234aa832573227081503fe
Burin Choomnuan <agilecreativity@gmail.com>
2016-05-06 13:36:04 +1000

    Add more examples

--- src/clojure/cfbt/ch08.clj
+++ src/clojure/cfbt/ch08.clj
@@ -215,3 +215,53 @@ l;; => "Great squid of Madrid, this is bad code: (1 + 1)
 ;; (= 1 2) was not successfull: false
 
 ;; Validation Function (p180)
+(def order-details
+  {:name "Michard Blimmons"
+   :email "michard.blimmons@gmail.com"})
+
+(def order-details-validations
+  {:name ["Please enter a name" not-empty]
+   :email ["Please enter an email address" not-empty
+    "Your email address doesn't look like an email address"
+    #(or (empty? %) (re-seq #"@" %))]})
+
+(defn error-messages-for
+  "Return a seq of error messages"
+  [to-validate message-validator-pairs]
+  (map first (filter #(not ((second %) to-validate))
+                     (partition 2 message-validator-pairs))))
+
+(error-messages-for "" ["Please enter a name" not-empty]) ;; ("Please enter a name")
+
+(defn validate
+  "Returns a map with a vector of errors for each key"
+  [to-validate validations]
+  (reduce (fn [errors validation]
+            (let [[fieldname validation-check-groups] validation value (get to-validate fieldname)
+                  error-messages (error-messages-for value validation-check-groups)]
+              (if (empty? error-messages)
+                errors
+                (assoc errors fieldname error-messages))))
+          {}
+          validations))
+
+(validate order-details order-details-validations)
+
+;; if-valid
+(defn if-valid
+  [record validations success-code failure-code]
+  (let [errors (validate order-details order-details-validations)]
+    (if (empty? errors)
+      (println :success)
+      (println :failure errors))))
+
+(defmacro if-valid
+  "Handle validation more concisely"
+  [to-validate validations errors-name & then-else]
+  `(let [~errors-name (validate ~to-validate ~validations)]
+     (if (empty? ~errors-name)
+       ~@then-else)))
+
+(;; if-valid order-details order-details-validation errors
+ ;;   (render :success)
+ ;;   (render :failure errors))
