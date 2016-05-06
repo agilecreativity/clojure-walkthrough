087dbc0961a615a85a7ebd08eabe8c7cd399ec82
Burin Choomnuan <agilecreativity@gmail.com>
2016-05-07 08:49:34 +1000

    Add more examples from ch09

--- src/clojure/cfbt/ch09.clj
+++ src/clojure/cfbt/ch09.clj
@@ -115,15 +115,36 @@
 ;; => Here's some Fereng wisdom:
 ;; =>  Whisper your way to success.
 
-;; Rolling Your Own Queue:: (p202)
+;; Rolling Your Own Queue::
 (defmacro wait
   "Sleep `timeout` seconds before evaluating body"
   [timeout & body]
   `(do (Thread/sleep ~timeout) ~@body))
 
-;; (let [saying3 (promise)]
-;;   (future (deliver saying3 (wait 100 "Cheerio!")))
-;;   @(let [saying2 (promise)]
-;;    (future (delivery saying2 (wait 400 "Pip pip!")))
-;;    @(let [saying 1 (promise)]
-;;       (future (deliver saying1 (wait 200 "Ello, gov'na!"))))))
+(let [saying3 (promise)]
+  (future (deliver saying3 (wait 100 "Cheerio")))
+  @(let [saying2 (promise)]
+     (future (deliver saying2 (wait 400 "Pip pip!")))
+     @(let [saying1 (promise)]
+        (future (deliver saying1 (wait 200 "Ello, gov'na!"))))))
+
+;; => "Ello, gov'na!"
+
+(defmacro enqueue
+  ([q concurrent-promise-name concurrent serialized]
+   `(let [~concurrent-promise-name (promise)]
+      (future (deliver ~concurrent-promise-name ~concurrent))
+      (deref ~q)
+      ~serialized
+      ~concurrent-promise-name))
+  ([concurrent-promise-name concurrent serialized]
+   `(enqueue (future) ~concurrent-promise-name ~concurrent ~serialized)))
+
+(time @(-> (enqueue saying (wait 200 "'Ello, gov'na!") (println @saying))
+           (enqueue saying (wait 400 "Pip pip!") (println @saying))
+           (enqueue saying (wait 100 "Cheerio!") (println @saying))))
+
+;;=> 'Ello, gov'na!
+;;=> Pip pip!
+;;=> Cheerio!
+;;=> "Elapsed time: 402.20443 msecs"
