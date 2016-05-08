a9791a3c19252e0bde5907ee7c2d327514e35faa
Burin Choomnuan <agilecreativity@gmail.com>
2016-05-09 08:52:02 +1000

    Add more examples

--- src/clojure/cfbt/ch10.clj
+++ src/clojure/cfbt/ch10.clj
@@ -192,4 +192,51 @@
 (future (dosync
          (commute counter (sleep-print-update 105 "Thread B" inc))))
 
-;; Vars :: 221
+;; Vars ::
+(def ^:dynamic *notification-address* "dobby@elf.org")
+
+(binding
+ [*notification-address* "test@elf.org"]
+  (str "address: " *notification-address*)) ;; "address: test@elf.org"
+
+(binding [*notification-address* "tester-1@elf.org"]
+  (str "address: " *notification-address*)) ;; "address: tester-1@elf.org"
+
+(str "address: " *notification-address*) ;; "address: dobby@elf.org"
+
+(defn notify
+  [message]
+  (str "TO: " *notification-address* "\n"
+       "MESSAGE: " message))
+
+(notify "I fell.")
+;;=> "TO: dobby@elf.org\nMESSAGE: I fell."
+
+;; Use binding
+(binding [*notification-address* "test@elf.org"]
+  (notify "test!"))
+;; => "TO: test@elf.org\nMESSAGE: test!"
+
+(binding [*out* (clojure.java.io/writer "print-output")]
+  (println "A man who carries a cat by the tail learns something he can learn in no other way. -- Mark Twain"))
+
+(slurp "print-output")
+;; => "A man who carries a cat by the tail learns something he can learn in no other way. -- Mark Twain\n"
+
+(def ^:dynamic *troll-thought* nil)
+(defn troll-riddle
+  [your-answer]
+  (let [number "man meat"]
+    (when (thread-bound? #'*troll-thought*)
+      (set! *troll-thought* number))
+    (if (= number your-answer)
+      "TROLL: You can cross the bridge!"
+      "TROLL: Time to eat you, succulent human!")))
+
+(binding [*troll-thought* nil]
+  (println (troll-riddle 2))
+  (println "SUCCULENT HUMAN: Ooooh! The answer was" *troll-thought*))
+;; => TROLL: Time to eat you, succulent human!
+;; => SUCCULENT HUMAN: Ooooh! The answer was man meat
+
+
